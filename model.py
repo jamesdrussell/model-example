5e39760ff5975034e9ac1e9bc6b21f42b652a275
James Russell <jamesdrus@gmail.com>
2016-03-10 12:30:24 -0800

    Adding sample model

---
+++ model.py
@@ -0,0 +1,9 @@
+def test_function_1(request):
+    return { 
+        'message' : 'test function 1!'
+    }
+
+def test_function_2(request):
+    return { 
+        'message' : 'test function 2!'
+    }
