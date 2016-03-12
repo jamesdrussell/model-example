8213165642054aba1f37ef701444947d39a4fd68
Rama <rama.shenai@gmail.com>
2016-03-12 10:54:11 -0800

    Attemp to get a query to work

--- et.py
+++ et.py
@@ -1,4 +1,7 @@
+import fb_query;
+
 def et_calc(request):
-    return { 
-        'message' : 'test function asdfasdfasdf!'
-    }
+	client = fb_query.FbApi('ghcn')
+	result = client.stations_by_radius_q(42.0347, -93.6200, 2, 100)
+	print result
+	return result.json()
