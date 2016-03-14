54f2a858ae454de6eb227b7d509f9bed64723dd3
Rama <rama.shenai@gmail.com>
2016-03-14 13:53:19 -0700

    Speed up

--- model.py
+++ model.py
@@ -3,13 +3,21 @@ import numpy as np
 import requests as req
 
 import et
+import time
+
+def test_et_compute():
+    start = time.time()
+    result = et_compute({"lat":42.021389, "lon":-93.77388, "days" : 365, "start-date" : "2012-01-01"})
+    end = time.time()
+    print (end-start)
 
 def et_compute(request):
-	lat = request['lat']
-	lon = request['lon']
-	etmodel = et.EtBlaneyCriddleModel()
-	data_series = etmodel.compute(lat, lon, None, 3290)
-	return pd.Series.to_dict(data_series['ET'])
+    lat = request['lat']
+    lon = request['lon']
+    ndays = request['days']
+    etmodel = et.EtBlaneyCriddleModel()
+    data_series = etmodel.compute(lat, lon, None, ndays)
+    return pd.Series.to_dict(data_series['ET'])
 
 def test_function_1(request):
     a = np.array([1, 2, 3])
